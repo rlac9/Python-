80d65e2847e747e85bf5f2cff1b9be5e46269190
co9 <rlaco91021@gmail.com>
2025-05-11 21:57:27 +0900

    Create list practice.py
    2025.5.11

---
+++ list practice.py
@@ -0,0 +1,8 @@
+# list practice
+# list calculation
+numbers = [1,12,123] 
+greeting = ['hi', 'hello','whatsup'] # list definition
+# +
+print(numbers + greeting) # : [1,12,123,'hi','hello','whatsup']
+# *
+print(number * 3) # :[1,12,123,1,12,123,1,12,123]
